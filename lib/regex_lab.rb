56dea8be08a0af144fe1e40bc3f5a23943bd079d
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-21 18:41:53 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,7 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
     if text.match(/\b[A-Z]\B/)
       true 
-    else
+    else if text.match9(/\b[a-z]\B/)
       false
     end 
 end
