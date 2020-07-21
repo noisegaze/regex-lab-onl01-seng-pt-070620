2303a1d5db36f8015dc76bdfc671d7ed432be1b8
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-21 19:28:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-if phone.match(//)
+if phone.match(/(\d+)-(\d+)-(\d+)|(\([0-9]*){10}|([0-9]{7})\b/)
   true
 else
   false
