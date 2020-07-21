640d68c4e4943812e423ebf36ef7fcc0df17a4dd
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-21 18:08:20 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,13 @@
 def starts_with_a_vowel?(word)
-
+if word.match(/\b[AEIOUaeiou]/)
+  true 
+ else 
+   false 
+ end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  
 end
 
 def words_five_letters_long(text)
